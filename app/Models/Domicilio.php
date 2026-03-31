f51118ce323c6a00ee225bb55e40613d4e54ad18
Alejandro Flores <umdalecs@proton.me>
2026-03-31 17:47:32 -0600

    laravel 13 syntax changes pt2

--- app/Models/Domicilio.php
+++ app/Models/Domicilio.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,15 +11,15 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 #[Table(
     'domicilios',
     key: 'rfc',
+    keyType: 'string',
     incrementing: false,
     timestamps: false
 )]
+#[Fillable('calle', 'numero', 'colonia', 'cp')]
 class Domicilio extends Model
 {
     use HasFactory;
 
-    protected $keyType = 'string';
-    protected $fillable = ["calle", "numero", "colonia", "cp"];
     public function persona(): BelongsTo
     {
         return $this->belongsTo(Persona::class, 'rfc', 'rfc');
--- app/Models/Persona.php
+++ app/Models/Persona.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,15 +11,16 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 #[Table(
     'personas',
     key: 'rfc',
+    keyType: 'string',
     incrementing: false,
     timestamps: false
 )]
+#[Fillable('nombre')]
 class Persona extends Model
 {
     use HasFactory;
 
     protected $keyType = 'string';
-    protected $fillable = ["nombre"];
     public function domicilio(): HasOne
     {
         return $this->hasOne(Domicilio::class, 'rfc', 'rfc');
